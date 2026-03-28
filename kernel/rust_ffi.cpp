31fd43e621dce815a199b00cf3d29789a1588b19
Andrey Smetanin <irqlevel@gmail.com>
2026-03-28 15:31:11 +0300

    rust checks and tag

--- kernel/rust_ffi.cpp
+++ kernel/rust_ffi.cpp
@@ -5,6 +5,8 @@
 #include <mm/page_allocator.h>
 #include <lib/stdlib.h>
 
+static const ulong RustAllocTag = 'rust';
+
 extern "C" {
 
 void kernel_trace(unsigned int level, const unsigned char* msg, unsigned long len)
@@ -20,8 +22,9 @@ void kernel_trace(unsigned int level, const unsigned char* msg, unsigned long le
 
 void* kernel_alloc(unsigned long size, unsigned long align)
 {
-    (void)align;
-    return Kernel::Mm::Alloc(size, 0);
+    if (align == 0 || (align & (align - 1)) != 0 || align > 8)
+        Panic("kernel_alloc: bad align %u", align);
+    return Kernel::Mm::Alloc(size, RustAllocTag);
 }
 
 void kernel_free(void* ptr)
